2c565d31dd4c938800bf47c66d57f8274fd91621
Peter Prescott <prescott.peter@gmail.com>
2021-02-26 16:35:52 +0000

    Adjust commas

--- gridsome.config.js
+++ gridsome.config.js
@@ -5,8 +5,8 @@
 // To restart press CTRL + C in terminal and run `gridsome develop`
 
 module.exports = {
-  siteName: 'Netlify CMS Gridsome starter template',
-  siteDescription: 'A simple, hackable & minimalistic starter for Gridsome that uses Netlify CMS for content.',
+  siteName: "Peter Prescott's Blog",
+  siteDescription: 'A simple, hackable & minimalistic blog.',
 
   templates: {
     Post: '/:title',
